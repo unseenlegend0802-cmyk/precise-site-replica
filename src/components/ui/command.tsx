d23d3bcf346bf788893f4393b67c01407998a949
unseenlegend0802-cmyk <unseenlegend0802@gmail.com>
2026-02-23 01:59:20 +0530

    Update command.tsx

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -21,19 +21,37 @@ const Command = React.forwardRef<
 ));
 Command.displayName = CommandPrimitive.displayName;
 
-interface CommandDialogProps extends DialogProps {}
+type CommandDialogProps = React.ComponentProps<typeof Dialog>
 
-const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
+export function CommandDialog({
+  children,
+  ...props
+}: CommandDialogProps) {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+        <Command
+          className="
+            [&_[cmdk-group-heading]]:px-2
+            [&_[cmdk-group-heading]]:font-medium
+            [&_[cmdk-group-heading]]:text-muted-foreground
+            [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0
+            [&_[cmdk-group]]:px-2
+            [&_[cmdk-input-wrapper]_svg]:h-5
+            [&_[cmdk-input-wrapper]_svg]:w-5
+            [&_[cmdk-input]]:h-12
+            [&_[cmdk-item]]:px-2
+            [&_[cmdk-item]]:py-3
+            [&_[cmdk-item]_svg]:h-5
+            [&_[cmdk-item]_svg]:w-5
+          "
+        >
           {children}
         </Command>
       </DialogContent>
     </Dialog>
-  );
-};
+  )
+}
 
 const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
@@ -121,7 +139,6 @@ CommandShortcut.displayName = "CommandShortcut";
 
 export {
   Command,
-  CommandDialog,
   CommandInput,
   CommandList,
   CommandEmpty,
